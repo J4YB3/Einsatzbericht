5a6e3542ae26485831926d6177df4c7861d5e1f4
Jan Bechtold <jan-bechtold@web.de>
2024-12-15 13:40:43 +0100

    feat(init): initial commit with running setup for electron, vue3 and vuetify3

---
+++ dist-electron/index.js
@@ -0,0 +1,18 @@
+import { app, BrowserWindow } from "electron";
+app.whenReady().then(() => {
+  const mainWindow = new BrowserWindow({
+    width: 1e3,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+  if (process.env.NODE_ENV === "development") {
+    mainWindow.loadURL("http://localhost:3000");
+  } else {
+    mainWindow.loadFile("./dist/index.html");
+  }
+});
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
